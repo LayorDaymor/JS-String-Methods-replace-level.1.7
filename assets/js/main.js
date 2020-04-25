92f0d5463abe150473989cafb00d7b3068e8f370
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 18:12:27 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var text = "Sam is good at codingschool";
+
+console.log(text.replace("good","bad"))
+let bad =text.replace("good","bad");
+
+console.log(text.replace("Sam","Tom"));
+let tom =text.replace("Sam","Tom");
+
+
+console.log(text.replace("codingschool","tennis"));
+let tennis =text.replace("codingschool","tennis");
+
+
+document.write("<h1>" + bad + "</h1>"+"</br>");
+document.write("<h1>" + tom + "</h1>"+"</br>");
+document.write("<h1>" + tennis + "</h1>"+"</br>");
+
+
+
+
+
+
+
+
+
+
+
